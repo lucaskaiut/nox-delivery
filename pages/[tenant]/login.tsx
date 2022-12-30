8c05681065ff701f9a21090fcecfbd0064e301e5
Lucas Kaiut <lucas.kaiut@gmail.com>
2022-12-30 18:02:56 -0300

    finished login screen

--- pages/[tenant]/login.tsx
+++ pages/[tenant]/login.tsx
@@ -1,24 +1,28 @@
-import { GetServerSideProps } from 'next';
-import Head from 'next/head';
-import { useEffect, useState } from 'react';
-import { Button } from '../../components/Button';
-import { Header } from '../../components/Header';
-import { Input } from '../../components/Input';
-import { useAppContext } from '../../contexts/AppContext';
-import { api as apiService } from '../../services/api';
-import styles from '../../styles/Login.module.css';
-import { Tenant } from '../../types/Tenant';
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { useAppContext } from "../../contexts/AppContext";
+import { api as apiService } from "../../services/api";
+import styles from "../../styles/Login.module.css";
+import { Tenant } from "../../types/Tenant";
 
 const Login = (props: Props) => {
   const { tenant, setTenant } = useAppContext();
 
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
 
   useEffect(() => {
-    setTenant(props.tenant)
-  }, []);
+    setTenant(props.tenant);
+  });
 
   return (
     <div className={styles.container}>
@@ -26,20 +30,67 @@ const Login = (props: Props) => {
         <title>Login | {props.tenant.name}</title>
       </Head>
       <Header backwardLink={`/${tenant?.slug}`} />
-      <Input value={email} placeholder="Digite seu E-Mail" onChange={setEmail}/>
-      <Input value={password} placeholder="Digite sua senha" onChange={setPassword} isPassword/>
-      <Button 
-        label="Entrar"
-        onClick={() => {}}
-      />
+      <div className={styles.header}>{tenant?.name}</div>
+      <div
+        className={styles.subtitle}
+        style={{ borderColor: tenant?.mainColor }}
+      >
+        Use as credenciais para realizar o login.
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.formArea}>
+        <div className={styles.inputArea}>
+          <Input
+            value={email}
+            placeholder="Digite seu E-Mail"
+            onChange={setEmail}
+          />
+        </div>
+        <div className={styles.inputArea}>
+          <Input
+            value={password}
+            placeholder="Digite sua senha"
+            onChange={setPassword}
+            isPassword
+          />
+        </div>
+        <div className={styles.inputArea}>
+          <Button label="Entrar" onClick={() => {}} filled />
+        </div>
+      </div>
+      <div
+        className={styles.forgotArea}
+        style={{ borderColor: tenant?.mainColor }}
+      >
+        Esqueceu sua senha?{" "}
+        <Link
+          href={`/${tenant?.slug}/forget`}
+          style={{
+            textDecoration: "none",
+            color: tenant?.mainColor,
+            fontWeight: "bold",
+          }}
+        >
+          <span>Clique aqui</span>
+        </Link>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.signUpArea}>
+        <Button
+          label="Quero me cadastrar"
+          onClick={() => {
+            router.push(`/${tenant?.slug}/signup`);
+          }}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default Login;
 
 interface Props {
-  tenant: Tenant
+  tenant: Tenant;
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -52,15 +103,15 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   if (!tenant) {
     return {
       redirect: {
-        destination: '/',
-        permanent: false
-      }
-    }
+        destination: "/",
+        permanent: false,
+      },
+    };
   }
 
   return {
     props: {
-      tenant
-    }
-  }
-}
+      tenant,
+    },
+  };
+};
